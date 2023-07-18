1861f0e2b90dc752be8729dddf530198d23a7e3d
senthilkumar63 <127287380+senthilkumar63@users.noreply.github.com>
2023-07-18 14:52:38 +0530

    Create SliderImage.js

---
+++ src/Components/Pages/Slider/SliderImage.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-anonymous-default-export */
+import First from "../assets/First.jpg";
+import Second from "../assets/Second.jpg";
+import Third from "../assets/Third.jpg";
+import Fourth from "../assets/Fourth.jpeg";
+import Fifth from "../assets/Fifth.jpeg";
+import Sixth from "../assets/Sixth.jpeg";
+import Seventh from "../assets/Seventh.jpeg";
+
+export default [
+  {
+    urls: First,
+  },
+  {
+    urls: Second,
+  },
+  {
+    urls: Third,
+  },
+  {
+    urls: Fourth,
+  },
+  {
+    urls: Fifth,
+  },
+  {
+    urls: Sixth,
+  },
+  {
+    urls: Seventh,
+  },
+];
