041073177c2106f8d4d77eb72ee0471858b12955
senthilkumar63 <127287380+senthilkumar63@users.noreply.github.com>
2023-07-18 14:54:57 +0530

    Create PageNotFound.js

---
+++ src/Components/Pages/PageNotFound.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../Components/Layout/Layout'
+
+const PageNotFound = () => {
+  return (
+    <Layout>
+          <br />
+          <br />
+          <br />
+          <br />
+      <h1>404 ERROR</h1>
+    </Layout>
+  )
+}
+
+export default PageNotFound
