7b137450756130c027c9481b4629c8809a913790
senthilkumar63 <127287380+senthilkumar63@users.noreply.github.com>
2023-07-18 15:03:11 +0530

    Create About.js

---
+++ src/Pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../Components/Layout/Layout'
+
+const About = () => {
+  return (
+    <Layout>
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1>About</h1>
+        
+    </Layout>
+  )
+}
+
+export default About
