7049fbbb764efbc4c47050958818472eb7a14032
senthilkumar63 <127287380+senthilkumar63@users.noreply.github.com>
2023-07-18 14:53:54 +0530

    Create Contact.js

---
+++ src/Components/Pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../Components/Layout/Layout'
+
+const Contact = () => {
+  return (
+    <Layout>
+        <br />
+        <br />m
+        <br />
+        <br />
+      <h1>Contact</h1>
+   
+    </Layout>
+  )
+}
+
+export default Contact
