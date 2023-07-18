4957f82a6ce68228195a948d18fad64ae27ebd4e
senthilkumar63 <127287380+senthilkumar63@users.noreply.github.com>
2023-07-18 14:54:12 +0530

    Create Home.js

---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from '../Components/Layout/Layout'
+import Slider from '../Pages/Slider/Slider'
+
+const Home = () => {
+  return (
+    <Layout>
+          <br />
+          <br />
+          <br />
+          <br />
+      
+      <Slider/>
+    </Layout>
+  )
+}
+
+export default Home
