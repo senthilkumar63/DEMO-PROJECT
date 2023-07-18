045cddbedc6860e6910de838cb543707a0b54e1f
senthilkumar63 <127287380+senthilkumar63@users.noreply.github.com>
2023-07-18 14:54:30 +0530

    Create Login.js

---
+++ src/Components/Pages/Login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../../src/Styles/Login.css'
+
+const Login = () => {
+  return (
+    <div>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+      <h1>Login</h1>
+      <input type="text" placeholder="username"/>
+      <input type='password' placeholder='password'/>
+      
+    </div>
+  )
+}
+
+export default Login
